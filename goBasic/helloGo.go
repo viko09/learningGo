2f298ff0c15f5286188060a217964e99ec9c4313
vikoluna <vmlm2509@gmail.com>
2021-12-12 23:24:31 -0600

    firstFile on Go

--- goBasic/helloGo.go
+++ goBasic/helloGo.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hello World")
-	
+func main() {
+	fmt.Println("Hello Yisus")
+
 }
+
 // This is a comment
 
-// to run this script
+// to run this script type: go run filename.go
